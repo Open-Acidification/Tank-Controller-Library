0bd1bc878d0a040f6d275f72a40bf219cfb8e13e
James Foster <github@jgfoster.net>
2020-10-11 21:15:05 -0700

    Start UI States.

---
+++ src/UIState/SetPHSetPoint.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "UIState.h"
+
+class SetPHSetPoint : UIState {
+public:
+  virtual void handleKey(char key) {}
+};
---
+++ src/UIState/UIState.h
@@ -0,0 +1,27 @@
+/*
+ * Abstract superclass for the TankController UI states
+ * Concrete subclass for the initial (start) state
+ */
+
+#pragma once
+
+#include "../TankControllerLib.h"
+
+class UIState {
+public:
+  UIState() {}
+  virtual ~UIState() {}
+  virtual void handleKey(char key) = 0;
+
+protected:
+  void changeState(UIState* state) {
+    TankControllerLib::singleton()->changeState(state);
+  }
+};
+
+class UIStart : UIState {
+public:
+  UIStart() {}
+  ~UIStart() {}
+  virtual void handleKey(char key);
+};
