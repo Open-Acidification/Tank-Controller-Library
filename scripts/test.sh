6fd47baf609d89871246c2f0287806258ba69dc2
James Foster <github@jgfoster.net>
2020-11-09 02:09:05 -0800

    Start toward using a different `arduino_ci` fork/branch.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 bundle config --local path vendor/bundle
 bundle install
-bundle exec arduino_ci_remote.rb  --skip-examples-compilation
+bundle exec arduino_ci.rb  --skip-compilation
+# bundle exec arduino_ci.rb  --skip-examples-compilation
