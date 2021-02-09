1d7613abce05288edef2e0c9ec8e2e4330e0ac51
James Foster <git@jgfoster.net>
2021-02-09 11:28:59 -0800

    Revert "All tests pass for TemperatureControl"
    This reverts commit 5f60d619b49861d06265999a435b7f0ae6ee5827.

--- test/TemperatureControlTest.cpp
+++ test/TemperatureControlTest.cpp
@@ -11,57 +11,25 @@ bundle exec arduino_ci.rb --skip-examples-compilation --testfile-select=Temperat
 */
 
 //  void Chiller::updateControl(double currentTemperature);   // void TemperatureControl::turnOn(bool newState) {
-unittest(chillerUpdateControlBeforeIntervalAndWithinDelta) {
+unittest(chillerUpdateControlWithinDelta) {
     Chiller chiller;
-    assertFalse(chiller.getCurrentSwitchState());
-    chiller.setTargetTemperature(20);
-    // delay(31000);
-    chiller.updateControl(20.04);
-    assertFalse(chiller.getCurrentSwitchState());
-}
-
-unittest(chillerUpdateControlbBeforeIntervalAndOutsideOfDelta) {
-    Chiller chiller;
-    assertFalse(chiller.getCurrentSwitchState());
-    chiller.setTargetTemperature(20);
-    // delay(31000);
-    chiller.updateControl(20.05);
-    assertFalse(chiller.getCurrentSwitchState());
-}
-
-unittest(chillerUpdateControlAfterIntervalAndWithinDelta) {
-    Chiller chiller;
-    assertFalse(chiller.getCurrentSwitchState());
-    chiller.setTargetTemperature(20);
+    chiller.setTargetTemperature(30.04);
     delay(31000);
-    chiller.updateControl(20.04);
-    assertFalse(chiller.getCurrentSwitchState());
+    chiller.updateControl(30);
+    assertEqual(true, chiller.getCurrentSwitchState);
 }
 
-unittest(chillerUpdateControlAfterIntervalAndOutsideDelta) {
+unittest(chillerUpdateControlbBeforeInterval) {
     Chiller chiller;
-    assertFalse(chiller.getCurrentSwitchState());
-    chiller.setTargetTemperature(20);
+    chiller.setTargetTemperature(30.05);
+    chiller.updateControl(30);
+    assertEqual(true, chiller.getCurrentSwitchState);
+}
+
+unittest(chillerUpdateControlbAfterInterval) {
+    Chiller chiller;
+    chiller.setTargetTemperature(30.05);
     delay(31000);
-    chiller.updateControl(20.05);
-    assertTrue(chiller.getCurrentSwitchState());
+    chiller.updateControl(30);
+    assertEqual(false, chiller.getCurrentSwitchState);
 }
-
-unittest(heaterUpdateControlWithinDelta) {
-    Heater heater;
-    assertFalse(heater.getCurrentSwitchState());
-    heater.setTargetTemperature(20);
-    heater.updateControl(19.96);
-    assertFalse(heater.getCurrentSwitchState());
-}
-
-unittest(heaterUpdateControlOutsideDelta) {
-    Heater heater;
-    assertFalse(heater.getCurrentSwitchState());
-    heater.setTargetTemperature(20);
-    heater.updateControl(19.95);
-    assertTrue(heater.getCurrentSwitchState());
-}
-
-
-unittest_main()
