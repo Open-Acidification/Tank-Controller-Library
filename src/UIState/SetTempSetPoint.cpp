b7c59b09bb9de4f995dae3544a3c066da72f337f
Wesley <wesleypacs@gmail.com>
2021-03-07 14:25:25 -0800

    Printing behavior

--- src/UIState/SetTempSetPoint.cpp
+++ src/UIState/SetTempSetPoint.cpp
@@ -4,7 +4,13 @@
 #include "SetTempSetPoint.h"
 
 #include "../Devices/EEPROM_TC.h"
+#include "../Devices/LiquidCrystal_TC.h"
 
 void SetTempSetPoint::setValue(double value) {
   EEPROM_TC::instance()->setTemp(value);
+
+  char output[16];
+  sprintf(output, "New Temp=%.2f", value);
+  LiquidCrystal_TC::instance()->writeLine(output, 1);
+  delay(1000);  // 1second
 }
