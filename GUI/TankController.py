304dc97e3765587cb490548041880569ed6610ac
James Foster <git@jgfoster.net>
2021-03-17 07:42:04 -0700

    Continuing refactoring of GUI.

--- GUI/TankController.py
+++ GUI/TankController.py
@@ -4,91 +4,98 @@ print('TankController version ', libTC.version())
 
 try:
     import wx
-    import wx.Grid as grid
 except ImportError:
     print('wxpython is not installed')
     exit()
 
 class TankController(wx.Frame):
-  labels = ['1', '2', '3', 'A',
-            '4', '5', '6', 'B',
-            '7', '8', '9', 'C',
-            '*', '0', '#', 'D']
-  def __init__(self, parent, title):
-    super(TankController, self).__init__(parent, title = title, size = (800,600))
-    self.InitUI()
-    self.Centre()
-    self.Show()
 
-  def InitUI(self):
-    panel = wx.Panel(self)
-    panel.Bind(wx.EVT_CHAR, self.Keyboard)
+    def __init__(self, parent, title):
+        super(TankController, self).__init__(
+            parent, title=title, size=(800, 600))
+        self.InitUI()
+        self.Centre()
+        self.Show()
 
-    mainSizer = wx.BoxSizer(wx.HORIZONTAL)
+    def InitUI(self):
+        panel = wx.Panel(self)
+        panel.Bind(wx.EVT_CHAR, self.Keyboard)
+        mainSizer = wx.BoxSizer(wx.HORIZONTAL)
+        mainSizer.Add(self.device(panel))
+        mainSizer.Add(self.eeprom(panel))
+        panel.SetSizer(mainSizer)
 
-    version = wx.StaticText(panel, label="Version " + libTC.version())
-    mainSizer.Add(version,1)
+    def device(self, panel):
+        deviceSizer = wx.StaticBoxSizer(
+            wx.VERTICAL, panel, label="Tank Controller v" + libTC.version())
+        deviceSizer.Add(self.liquidCrystal(panel))
+        deviceSizer.Add(self.keypad(panel))
+        return deviceSizer
 
-    lqdSizer = wx.StaticBoxSizer(wx.VERTICAL, panel)
-    self.lqd = wx.StaticText(panel, label = libTC.lcd(0) + '\n' + libTC.lcd(1))
-    font = wx.Font(20, wx.FONTFAMILY_TELETYPE, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
-    self.lqd.SetFont(font)
-    lqdSizer.Add(self.lqd, 1)
-    mainSizer.Add(lqdSizer,1)
+    def liquidCrystal(self, panel):
+        lqdSizer = wx.StaticBoxSizer(wx.VERTICAL, panel)
+        self.lqd = wx.StaticText(
+            panel, label=libTC.lcd(0) + '\n' + libTC.lcd(1))
+        font = wx.Font(20, wx.FONTFAMILY_TELETYPE,
+                       wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
+        self.lqd.SetFont(font)
+        lqdSizer.Add(self.lqd)
+        return lqdSizer
 
-    keypadSizer = wx.StaticBoxSizer(wx.VERTICAL, panel)
+    def keypad(self, panel):
+        labels = ['1', '2', '3', 'A',
+                '4', '5', '6', 'B',
+                '7', '8', '9', 'C',
+                '*', '0', '#', 'D']
+        keypadSizer = wx.StaticBoxSizer(wx.VERTICAL, panel)
+        keypadGrid = wx.GridSizer(4, 4, 5, 5)
+        for each in labels:
+            button = wx.Button(panel, label=each)
+            button.Bind(wx.EVT_LEFT_UP, self.KeypadEvent)
+            keypadGrid.Add(button, 0, wx.EXPAND)
+        keypadSizer.Add(keypadGrid)
+        return keypadSizer
 
-    keypadGrid = wx.GridSizer(4, 4, 5, 5)
-    for each in self.labels:
-      button = wx.Button(panel, label = each)
-      button.Bind(wx.EVT_LEFT_UP, self.KeypadEvent)
-      keypadGrid.Add(button, 0, wx.EXPAND)
-    keypadSizer.Add(keypadGrid, 1)
-    mainSizer.Add(keypadSizer, 2)
+    # getPH();
+    # getTemp();
+    # getTankID();
+    # getCorrectedTemp();
+    # getKP();
+    # getKI();
+    # getKD();
+    # getMac();  // See issue #57 about this function
+    # getHeat();
+    # getAmplitude();
+    # getFrequency();
+    # getGranularity();
+    # getMaxDataAge();
+    # getPHSeriesSize();
+    # getPHSeriesPointer();
+    # getTempSeriesSize();
+    # getTempSeriesPointer();
+    # getPHInterval();
+    # getPHDelay();
+    # getTempInterval();
+    # getTempDelay();
 
-    addEEPROM(panel, mainSizer)
+    def eeprom(self, panel):
+        eepromSizer = wx.StaticBoxSizer(wx.VERTICAL, panel, label="EEPROM")
+        label = wx.StaticText(panel, label="PH")
+        eepromSizer.Add(label)
+        return eepromSizer
 
-    panel.SetSizer(mainSizer)
+    def KeypadEvent(self, event):
+        key = event.GetEventObject().GetLabel()
+        libTC.key(key)
+        libTC.loop()
+        self.lqd.SetLabelText(libTC.lcd(0) + '\n' + libTC.lcd(1))
+        print("Keypad", key)
 
-  # getPH();
-  # getTemp();
-  # getTankID();
-  # getCorrectedTemp();
-  # getKP();
-  # getKI();
-  # getKD();
-  # getMac();  // See issue #57 about this function
-  # getHeat();
-  # getAmplitude();
-  # getFrequency();
-  # getGranularity();
-  # getMaxDataAge();
-  # getPHSeriesSize();
-  # getPHSeriesPointer();
-  # getTempSeriesSize();
-  # getTempSeriesPointer();
-  # getPHInterval();
-  # getPHDelay();
-  # getTempInterval();
-  # getTempDelay();
+    def Keyboard(self, event):
+        key = chr(event.GetUnicodeKey()).upper()
+        print("Keyboard", key)
 
-  def addEEPEOM(self, panel, mainSizer):
-    eepromSizer = wx.StaticBoxSizer(wx.VERTICAL, panel, label="EEPROM")
-    label = wx.StaticText(panel, label = "PH")
-    eepromSizer.Add(label, 1)
-    mainSizer.Add(eepromSizer, 3)
-
-  def KeypadEvent(self, event):
-    key = event.GetEventObject().GetLabel()
-    libTC.key(key)
-    libTC.loop()
-    self.lqd.SetLabelText(libTC.lcd(0) + '\n' + libTC.lcd(1))
-    print("Keypad", key)
-
-  def Keyboard(self, event):
-    key = chr(event.GetUnicodeKey()).upper()
-    print("Keyboard", key)
-
-app = wx.App()
-TankController(None, title = 'Tank Controller')
-app.MainLoop()
+if __name__ == "__main__":
+    app = wx.App()
+    TankController(None, title='Tank Controller')
+    app.MainLoop()
