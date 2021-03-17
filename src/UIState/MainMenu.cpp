16e5b8893436afe99e66b9122e81edcdbe344e81
James Foster <git@jgfoster.net>
2021-03-16 17:38:13 -0700

    Now able to set Tank ID.

--- src/UIState/MainMenu.cpp
+++ src/UIState/MainMenu.cpp
@@ -2,7 +2,7 @@
 
 #include "CalibrationManagement.h"
 #include "ChangeGoogleSheetInterval.h"
-#include "ChangeTankID.h"
+#include "SetTankID.h"
 #include "EnablePID.h"
 #include "ManualSetTime.h"
 #include "PHCalibration.h"
@@ -35,7 +35,7 @@ void MainMenu::handleKey(char key) {
       this->setNextState((UIState*)new CalibrationManagement(tc));
       break;
     case '#':  // Change Tank ID
-      this->setNextState((UIState*)new ChangeTankID(tc));
+      this->setNextState((UIState*)new SetTankID(tc));
       break;
     case '*':  // Change Google Sheet Interval
       this->setNextState((UIState*)new ChangeGoogleSheetInterval(tc));
