78c65d8a58c15acda01ee6be87f25a7c2bb382f5
James Foster <git@jgfoster.net>
2021-03-22 20:12:40 -0700

    Fix transitions through Wait back to MainMenu.

--- test/SetChillOrHeat.cpp
+++ test/SetChillOrHeat.cpp
@@ -41,9 +41,12 @@ unittest(switchToChill) {
   // during the delay we showed the new value
   std::vector<String> lines = LiquidCrystal_TC::instance()->getLines();
   assertEqual("Use chiller     ", lines[1]);
+  assertEqual("SetChillOrHeat", tc->stateName());
+  tc->loop();  // transition to Wait
   assertEqual("Wait", tc->stateName());
   delay(1000);
-  tc->loop();
+  tc->loop();  // queue MainMenu to be next
+  tc->loop();  // transition to MainMenu
   // now we should be back to the main menu
   assertEqual("MainMenu", tc->stateName());
 }
--- test/SetPHSetPoint.cpp
+++ test/SetPHSetPoint.cpp
@@ -18,9 +18,12 @@ unittest(test) {
   // during the delay we showed the new value
   std::vector<String> lines = LiquidCrystal_TC::instance()->getLines();
   assertEqual("New pH=7.1234   ", lines[1]);
+  assertEqual("SetPHSetPoint", tc->stateName());
+  tc->loop();  // transition to Wait
   assertEqual("Wait", tc->stateName());
   delay(1000);
-  tc->loop();
+  tc->loop();  // queue MainMenu to be next
+  tc->loop();  // transition to MainMenu
   // now we should be back to the main menu
   assertEqual("MainMenu", tc->stateName());
 }
